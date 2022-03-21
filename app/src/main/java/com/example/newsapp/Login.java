ba6ff3a70235e1da202983381baca79708a91424
admin <tranxuanhiepcmd3@gmai.com>
2022-03-21 11:06:47 +0700

    update

--- app/src/main/java/com/example/newsapp/Login.java
+++ app/src/main/java/com/example/newsapp/Login.java
@@ -1,15 +1,34 @@
 package com.example.newsapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 
 public class Login extends Activity {
+
+    private RelativeLayout btnLogin;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        btnLogin = (RelativeLayout) findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextPage();
+            }
+
+            public void nextPage(){
+                Intent intent = new Intent(Login.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
